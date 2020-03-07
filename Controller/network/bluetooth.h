dbeeed7c34b6ff04f511d7b923d07e1c8daab5f1
Nightly <makatu1999@icloud.com>
2020-03-07 17:08:41 +0900

    FIRST COMMITgit add .

---
+++ Controller/network/bluetooth.h
@@ -0,0 +1,13 @@
+// bluetooth.h
+// author: tari
+
+BluetoothSerial SerialBT;
+
+void init_bt(){
+	SerialBT.begin("ESP32 Ducks"); //Bluetooth device name
+	Serial.println("I Duck. Welcome. You can connect me: Bluetooth, now!");
+}
+
+void send_bt(char content_send){
+	SerialBT.print(content_send);
+}
