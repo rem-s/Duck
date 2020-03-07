a9fbe47d88e217e26d89eba9a2450faf1d7bbacc
Aki49 <Akawabe049@gmail.com>
2020-03-07 20:36:25 +0900

    third comit

--- Raspi/control/sensor/acc.py
+++ Raspi/control/sensor/acc.py
@@ -1,59 +1,31 @@
-import RPi.GPIO as GPIO
 import time
+import board
+import busio
+import adafruit_adxl34x
+
 import csv
-import numpy as np
-
-trigger_pin = 17
-echo_pin = 23
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(trigger_pin, GPIO.OUT)
-GPIO.setup(echo_pin, GPIO.IN)
 
 class acc:
     def __init__(self):
-        pass
+        i2c = busio.I2C(board.SCL, board.SDA)
+        accelerometer = adafruit_adxl34x.ADXL345(i2c)
 
-    def send_trigger_pulse():
-        GPIO.output(trigger_pin, True)
-        time.sleep(0.0001)
-        GPIO.output(trigger_pin, False)
-
-    def wait_for_echo(value, timeout):
-        count = timeout
-        while GPIO.input(echo_pin) != value and count > 0:
-            count = count - 1
-
-    def get_distance():
-        send_trigger_pulse()
-        wait_for_echo(True, 10000)
-        start = time.time()
-        wait_for_echo(False, 10000)
-        finish = time.time()
-        pulse_len = finish - start
-        distance_cm = pulse_len / 0.0000580
-        distance_in = distance_cm / 2.5
-        #return (distance_cm, distance_in)
-        return distance_cm
-
-    def getAcc():
-        distance_cm = get_distance()
-        return distance_cm
+    def get_acceleration(self):
+        x_axis, y_axis, z_axis = accelerometer.acceleration
+        return [x_axis, y_axis, z_axis]
 
 if __name__ == "__main__":
-    print('hello')
-
     tmp = acc()
-
-    f = open('distance.csv', 'w')
+    f = open('accelerometer.csv', 'w')
 
     #while True:
     for _ in range(20):
-        #print("cm=%f\tinches=%f" % tmp.get_distance())
-        distance_cm = tmp.get_distance()
-        print("cm=", distance_cm)
+        x_axis, y_axis, z_axis = tmp.get_acceleration()
+        #print("%f %f %f"%tmp.get_acceleration())
         writer = csv.writer(f, lineterminator='\n')
-        writer.writerow([distance_cm])
+        writer.writerow([x_axis, y_axis, z_axis])
+        print('{:.6f} {:.6f} {:.6f}'.format(x_axis, y_axis, z_axis))
         time.sleep(1)
 
     f.close()
+
--- Raspi/control/sensor/sonar.py
+++ Raspi/control/sensor/sonar.py
@@ -1,8 +1,53 @@
-import numpy as np
+# import RPi.GPIO as GPIO
+# import time
+# import csv
+# import numpy as np
 
-class acc:
+# trigger_pin = 17
+# echo_pin = 23
+
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(trigger_pin, GPIO.OUT)
+# GPIO.setup(echo_pin, GPIO.IN)
+
+class sonar:
     def __init__(self):
         pass
 
+    def send_trigger_pulse():
+        GPIO.output(trigger_pin, True)
+        time.sleep(0.0001)
+        GPIO.output(trigger_pin, False)
+
+    def wait_for_echo(value, timeout):
+        count = timeout
+        while GPIO.input(echo_pin) != value and count > 0:
+            count = count - 1
+
+    def get_distance():
+        send_trigger_pulse()
+        wait_for_echo(True, 10000)
+        start = time.time()
+        wait_for_echo(False, 10000)
+        finish = time.time()
+        pulse_len = finish - start
+        distance_cm = pulse_len / 0.0000580
+        distance_in = distance_cm / 2.5
+        #return (distance_cm, distance_in)
+        return distance_cm
+
 if __name__ == "__main__":
-    print('hello')
+    tmp = sonar()
+
+    f = open('distance.csv', 'w')
+
+    #while True:
+    for _ in range(20):
+        #print("cm=%f\tinches=%f" % tmp.get_distance())
+        distance_cm = tmp.get_distance()
+        print("cm=", distance_cm)
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerow([distance_cm])
+        time.sleep(1)
+
+    f.close()
