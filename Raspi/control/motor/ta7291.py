a2bfd0f5491da47ca0bfa1cfa72ff3399040f2bd
HayatoKato <aaaaaaaaainjgvd@gmail.com>
2020-03-07 16:37:41 +0900

    add udp and motor ta7291p

--- Raspi/control/motor/ta7291.py
+++ Raspi/control/motor/ta7291.py
@@ -1,1 +1,54 @@
-# about ta7291 class
+#About TA7291 class
+import RPi.GPIO as GPIO
+
+class TA7291P:
+	def __init__(self, 
+				 in1, 
+				 in2, 
+				 pwm):
+				 
+		#setting ports
+		self.in1 = in1
+		self.in2 = in2
+		self.pwm = pwm
+		
+		#setting state
+		self.state = 2
+		self.pwm.start(0)
+		
+	def stop(self):
+	
+		#setting stop motor
+		GPIO.output(self.in1, 0)
+		GPIO.output(sefl.in2, 0)
+		pwm.ChangeDutyCycle(0)
+	
+		#setting stop mode state
+		self.state = 2
+		
+	def set_motor_pwm(self, 
+					  param):
+		
+		#setting car direction
+		now_state = param > 0
+		if self.state != now_state:
+			if now_state: self.set_straight()
+			else: self.set_back()
+			self.state = now_state
+		
+		#setting car speed
+		pwm_power = abs(param) * 100
+		pwm.ChangeDutyCycle(pwm_power)
+		
+	def set_straight(self):
+	
+		#setting straight
+		GPIO.output(self.in1, 1)
+		GPIO.output(self.in2, 0)
+	
+	def set_back(self):
+	
+		#setting back
+		GPIO.output(self.in1, 0)
+		GPIO.output(self.in2, 1)
+		
--- Raspi/network/udp.py
+++ Raspi/network/udp.py
@@ -1,1 +1,31 @@
-# udp class
+# UDP class
+from socket import socket, AF_INET, SOCK_DGRAM
+
+class UDP:
+	def __init__(self,
+				master_ip,
+				slave_ip,
+				sport,
+				rport):
+				
+		#set comunication information
+		self.master_ip = master_ip
+		self.slave_ip = slave_ip
+		self.sport = sport
+		self.rport = rport
+		
+		#set and connect socket
+		self.socket = socket(AF_INET, SOCK_DGRAM)
+		self.socket.bind((self.slave_ip, self.rport))
+		
+	def receive(self):
+		
+		#receiving data
+		data, addr = self.socket.recvfrom(1024)
+		return data
+		
+	def send(self, data):
+	
+		#sending data
+		self.socket.sendto(data, (self.slave_ip, self.sport))
+		
