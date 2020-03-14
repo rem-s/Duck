d21971245aea3a91c02d481a696b00d87baf8f58
T Ll <makatu1999@icloud.com>
2020-03-14 18:24:59 +0900

    t sh

---
+++ Controller/network/sketch_mar14a/sketch_mar14a.ino
@@ -0,0 +1,16 @@
+//#include "bluetooth.h"
+//#include "udp.h"
+//#include "wifi_ducks.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  //Serial.begin(9600);
+  //init_wifi();
+  //init_bt();
+  //send_bt('T');
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
