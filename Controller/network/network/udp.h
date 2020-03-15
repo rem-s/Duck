95e58fd7fabfcefedc8f9d3f26a294bc4e218d90
Tollokj Llatsvkavva <s1250039@u-aizu.ac.jp>
2020-03-16 01:35:56 +0900

    Signed-off-by: Tollokj Llatsvkavva <s1250039@u-aizu.ac.jp>

--- Controller/network/network/udp.h
+++ Controller/network/network/udp.h
@@ -9,7 +9,7 @@ static char* addr_ip_remote = "0.0.0.0"; //IP address, remotes.
 static WiFiUDP wifiUdp;
 
 void init_udp(int port, char* addr) {
-  Serial.println("Init UDP.");
+  Serial.println("I will initialize service UDP.");
   port_udp_remote = port;
   addr_ip_remote = addr;
   wifiUdp.begin(port_udp_remote);
@@ -17,12 +17,12 @@ void init_udp(int port, char* addr) {
 }
 
 void send_udp(char content_send) {
-  Serial.println("I will send something to client, content is");
+  Serial.println("I will send a character to the client, it is");
   Serial.println("----------");
   Serial.println(content_send);
   Serial.println("----------");
   wifiUdp.beginPacket(addr_ip_remote, port_udp_remote);
   wifiUdp.write(content_send);
   wifiUdp.endPacket();
-  Serial.println("Sent.");
+  Serial.println("I sent that.");
 }
