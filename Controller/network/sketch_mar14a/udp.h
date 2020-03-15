6af21ca175c8b254538a2ad410e253b2d2e0c4f8
Tari Tari <makatu1999@icloud.com>
2020-03-15 18:44:49 +0900

    t sh

--- Controller/network/sketch_mar14a/udp.h
+++ Controller/network/sketch_mar14a/udp.h
@@ -3,20 +3,26 @@
 
 //--- Constant, define by user
 static const char* addr_ip_remote = "192.168.0.56"; //IP address, remotes.
-static const int port_udp_remote = 8888; // UDP port, remotes.
 //---
 
 #include <WiFi.h>
 
+static int port_udp_remote = 0; // UDP port, remotes.
+static char* addr_ip_remote = "0.0.0.0"; //IP address, remotes.
+
 static WiFiUDP wifiUdp;
 
-void init_udp(int port){
-	wifiUdp.begin(port);
-  Serial.println("UDP Started.");
+void init_udp(int port, int addr) {
+  Serial.println("Init UDP.");
+  port_udp_remote = port;
+  addr_ip_remote = addr;
+  wifiUdp.begin(port_udp_remote);
+  Serial.println("Service UDP started.");
 }
 
-void send_udp(char content_send){
-	wifiUdp.beginPacket(addr_ip_remote, port_udp_remote);
-	wifiUdp.write(content_send);
-	wifiUdp.endPacket();
+void send_udp(char content_send) {
+  wifiUdp.beginPacket(addr_ip_remote, port_udp_remote);
+  wifiUdp.write(content_send);
+  wifiUdp.endPacket();
+  Serial.println("Service UDP started.");
 }
