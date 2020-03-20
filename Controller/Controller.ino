ee1ef7794895a7cbc77c887b2f19f192e68d5282
Tari Tari <makatu1999@icloud.com>
2020-03-20 17:09:00 +0900

    home made Shinchoku

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -10,11 +10,21 @@ void setup() {
   init_wifi();
   //init_bt();
   init_udp(8888, "192.168.0.56");
-  init_button(4);
+  init_button(0,16);
+  init_button(1,0);
+  init_button(2,0);
+  init_button(3,0);
+  init_joy_stick(,);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  send_udp('T');
-  get_status_button(4);
+  static String sendval;
+  if (get_status_button(0) == 1) {
+    send_udp('F');
+    delay(50);
+  }
+  get_value_stick_x()
+  get_value_stick_y()
+  
 }
