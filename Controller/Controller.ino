e1f333f3ec1ff0a1b7b6b3bd26bc42db55e3b7b6
Tari Tari <makatu1999@icloud.com>
2020-03-20 22:23:33 +0900

    Shinchoku, home made.

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,3 +1,20 @@
+/* controller.ino
+ * shinchokuer: tari
+ * 
+ * This, program for controller, ESP32 based.
+ * This will read input value of buttons and joyeuxsticke, then send
+ * some commands to Duck Raspberry Pi via UDP connection over Wi-Fi.
+ * 
+ * COMMANDS:
+ * 
+ * FRONT: 1
+ * BACK: 2
+ * LEFT: 3
+ * RIGHT: 4
+ * STOP: 0
+ */
+
+
 //#include "network/bluetooth.h"
 #include "network/udp.h"
 #include "network/wifi_ducks.h"
@@ -9,7 +26,7 @@ void setup() {
   Serial.begin(9600);
   init_wifi();
   //init_bt();
-  ///init_udp(8888, "192.168.0.56");
+  init_udp(8889, "192.168.0.58"); // (port, address)
   init_button(0, 16);
   init_button(1, 0);
   init_button(2, 0);
@@ -24,17 +41,20 @@ void loop() {
     //send_udp("F");
     //delay(50);
   }
-  if (get_value_stick_x() > 1024) {
-    //send_udp('R');
+  if (get_value_stick_x() < -1024) {
+    send_udp('4');
   }
-  else if (get_value_stick_x() < -1024) {
-    //send_udp('L');
+  else if (get_value_stick_x() > 1024) {
+    send_udp('3');
+  }
+  else if (get_value_stick_y() < -1024) {
+    send_udp('1');
   }
   else if (get_value_stick_y() > 1024) {
-    //send_udp('F');
+    send_udp('2');
   }
-  else if (get_value_stick_x() < -1024) {
-    //send_udp('B');
+  else {
+    send_udp('0');
   }
   delay(200);
 }
