d44add8748c2acb58f6221143c4ae04e774fb6c9
Tari Tari <makatu1999@icloud.com>
2020-03-30 00:11:56 +0900

    remove passphrase string

--- Controller/network/wifi_ducks.h
+++ Controller/network/wifi_ducks.h
@@ -3,18 +3,21 @@
 
 //--- Constant, define by user
 const char ssid_wifis[] = "PERL122D-REMs"; // SSID APs.
-const char pass_wifis[] = "duckduck";  // Password APs.
+const char pass_wifis[8];  // Password APs.
 //---
 
 #include <WiFi.h>
 
 void init_wifi(void) {
-  Serial.println("Connecting wireless, Start.");
-	WiFi.begin(ssid_wifis, pass_wifis);
-  	while( WiFi.status() != WL_CONNECTED) {
-		delay(500);
-	}
+    Serial.println("Connecting wireless, Start.");
+    for(n = 0 ; n < 8 ; n++) {
+        pass_wifis[n] = Serial.read();
+    }
+    WiFi.begin(ssid_wifis, pass_wifis);
+    while( WiFi.status() != WL_CONNECTED) {
+        delay(500);
+    }
     disp_string("SSID: ");
     disp_string(ssid_wifis);
- Serial.println("Connected, Wireless.");
+    Serial.println("Connected, Wireless.");
 }
