c9716bf9be0b23e07dd4be9229ba706be2318214
tari <s1250039@u-aizu.ac.jp>
2020-05-05 03:30:03 +0900

    NightlyCanary
    fix typo

--- Controller/network/wifi_ducks.h
+++ Controller/network/wifi_ducks.h
@@ -16,7 +16,7 @@ void init_wifi(void) {
 	WiFi.begin(SSID_WIFIS, PASS_WIFIS);					// Defined at passwd.h, is added to .gitignore so this header file is not on GitHub.
 	//Serial.println("begin");
 	for(n = 0 ; n <= 4 ; n++) {		  					// Retry count: 4
-		if(WiFi.status() != WL_CONNECTED) {
+		if(WiFi.status() == WL_CONNECTED) {
 			break;
 		}
 		if(WiFi.status() != WL_CONNECTED){
@@ -29,7 +29,6 @@ void init_wifi(void) {
 			}
 			delay(500);
 		}
-		
 	}
 	disp_stringln("          [OK]");
 	disp_string("SSID: ");
